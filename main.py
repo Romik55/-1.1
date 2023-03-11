03d39a9993db569a768331ee25656677f662ccab
Romik 55 <romanbida04052018@gmail.com>
2023-03-11 20:19:48 +0200

    дз2

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Привіт Ігоре я роблю домашнє :)")
+class dog:
 
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+
+    def info(self):
+        name=self.name
+        age=self.age
+        print(f"Dog:{name} age:{age}")
+
+d1 = dog("lord", 4)
+d1.info()
+d2 = dog("rex", 1)
+d2.info()
